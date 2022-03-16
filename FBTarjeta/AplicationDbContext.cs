86180240cbc4f76882050593524b4dbd01c498bf
Lucas-De-Nicola <65104445+Lucas-De-Nicola@users.noreply.github.com>
2022-03-16 15:50:14 -0300

    BackEnd

---
+++ FBTarjeta/AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FBTarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjeta
+{
+    public class AplicationDbContext: DbContext
+    {
+        //mapeando nuestro modelo con la base de datos
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
